b21e2aa3152ee6ca9c520c26f645057dc1034852
Beny.K <Beku1997@gmail.com>
2023-07-20 15:46:23 +0300

    full working , upgrades and updates needed

---
+++ actions/get-sales-count.ts
@@ -0,0 +1,13 @@
+
+import prismadb from "@/lib/prisma.db";
+
+export const getSaleCount = async (storeId:string) =>{
+    const salesCount = await prismadb.order.count({
+        where:{
+            storeId,
+            isPaid:true
+        }
+    })
+
+    return salesCount
+}
